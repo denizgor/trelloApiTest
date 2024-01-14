b5f9c1759c383ec788ce8a85acad8a5942792e33
denizgor <71087556+denizgor@users.noreply.github.com>
2024-01-15 02:12:18 +0300

    - check_key_and_value method was created

--- base/base_functions.py
+++ base/base_functions.py
@@ -9,6 +9,20 @@ class Base(unittest.TestCase, object):
         request_response = requests.request("{}".format(method), url, json=payload, **kwargs)
         return request_response
 
-    def check_data_type(self, data_type, value):
-        return type(value) == data_type
+    # def check_data_type(self, data_type, value):
+    #     return type(value) == data_type
 
+    def check_key_and_value(self, key_in_response, response, payload_variable = None):
+        self.assertTrue(key_in_response in response, "Error! {} is not in the response.".format(key_in_response))
+        self.assertTrue(key_in_response is not None and key_in_response != "", "{} is null".format(key_in_response))
+
+        if payload_variable is not None:
+            self.assertEqual(payload_variable, key_in_response, "{} and the {} do not match!"
+                             .format(payload_variable, key_in_response))
+
+        print("The key is present in the response and the value for the key is valid and correct.")
+    def check_data_type(self, data_type, key_in_response):
+        try:
+            return type(key_in_response) == data_type
+        except AssertionError as error:
+            print("Assertion error: {}".format(error))
--- tests/test_create_board.py
+++ tests/test_create_board.py
@@ -1,6 +1,5 @@
 import base.base_urls
 from base.base_functions import Base
-from base.base_urls import BaseUrls
 from base.access_token import AccessToken
 import json
 
@@ -20,122 +19,27 @@ class TestCreateBoard(Base):
 
         self.assertEqual(response.status_code, 200, "Status code is NOT 200!")
         self.assertEqual(response.headers["Content-Type"], "application/json; charset=utf-8", "Incorrect JSON format!")
-        #self.assertEqual(response.json(), self.board_payload, "Response does not match the request!")
 
         response_json = response.json()
+        board_name = response_json["name"]
+        board_id = response_json["id"]
 
         self.assertTrue(self.check_data_type(dict, response_json), "Response type is not dictionary!")
-        self.assertTrue(response_json["name"] is not None)
-        self.assertTrue(self.check_data_type(str, response_json["name"]),"name type is not string")
+
+        self.check_data_type(str, board_name)
+        self.check_key_and_value(board_name,self.board_payload["name"])
+
         self.assertEqual(response_json["name"], self.board_payload["name"], "Board name doesn't match!")
 
-        print("Board Variable Name:", self.board_payload["name"], "Created Board Name: ", response_json["name"])
-        print(type(response_json["name"]))
+        # print("Board Variable Name:", self.board_payload["name"], "Created Board Name: ", response_json["name"])
+        # print(type(response_json["name"]))
 
-        self.assertTrue("id" in response_json, "Board id is not present in the response!")
-        self.assertTrue(response_json["id"] is not None, "Error: Board id is null!")
+        self.check_key_and_value("id", response_json)
+        # self.assertTrue("id" in response_json, "Board id is not present in the response!")
+        # self.assertTrue(response_json["id"] is not None, "Error: Board id is null!")
 
         self.assertTrue(response_json["closed"] is False, "Error: Board is closed!")
 
         self.assertTrue(self.check_data_type(str, response_json["prefs"]["background"]), "background type is not string")
 
 
-#
-# {
-#   "id": "659b19552aa427f06c079e2a",
-#   "name": "Test4",
-#   "desc": "",
-#   "descData": null,
-#   "closed": false,
-#   "idOrganization": "65904fdb10984513ce794021",
-#   "idEnterprise": null,
-#   "pinned": false,
-#   "url": "https://trello.com/b/quyAhk3V/test4",
-#   "shortUrl": "https://trello.com/b/quyAhk3V",
-#   "prefs": {
-#     "permissionLevel": "private",
-#     "hideVotes": false,
-#     "voting": "disabled",
-#     "comments": "members",
-#     "invitations": "members",
-#     "selfJoin": true,
-#     "cardCovers": true,
-#     "cardCounts": false,
-#     "isTemplate": false,
-#     "cardAging": "regular",
-#     "calendarFeedEnabled": false,
-#     "hiddenPluginBoardButtons": [],
-#     "switcherViews": [
-#       {
-#         "viewType": "Board",
-#         "enabled": true
-#       },
-#       {
-#         "viewType": "Table",
-#         "enabled": true
-#       },
-#       {
-#         "viewType": "Calendar",
-#         "enabled": false
-#       },
-#       {
-#         "viewType": "Dashboard",
-#         "enabled": false
-#       },
-#       {
-#         "viewType": "Timeline",
-#         "enabled": false
-#       },
-#       {
-#         "viewType": "Map",
-#         "enabled": false
-#       }
-#     ],
-#     "background": "blue",
-#     "backgroundColor": "#0079BF",
-#     "backgroundImage": null,
-#     "backgroundTile": false,
-#     "backgroundBrightness": "dark",
-#     "backgroundImageScaled": null,
-#     "backgroundBottomColor": "#0079BF",
-#     "backgroundTopColor": "#0079BF",
-#     "canBePublic": true,
-#     "canBeEnterprise": true,
-#     "canBeOrg": true,
-#     "canBePrivate": true,
-#     "canInvite": true
-#   },
-#   "labelNames": {
-#     "green": "",
-#     "yellow": "",
-#     "orange": "",
-#     "red": "",
-#     "purple": "",
-#     "blue": "",
-#     "sky": "",
-#     "lime": "",
-#     "pink": "",
-#     "black": "",
-#     "green_dark": "",
-#     "yellow_dark": "",
-#     "orange_dark": "",
-#     "red_dark": "",
-#     "purple_dark": "",
-#     "blue_dark": "",
-#     "sky_dark": "",
-#     "lime_dark": "",
-#     "pink_dark": "",
-#     "black_dark": "",
-#     "green_light": "",
-#     "yellow_light": "",
-#     "orange_light": "",
-#     "red_light": "",
-#     "purple_light": "",
-#     "blue_light": "",
-#     "sky_light": "",
-#     "lime_light": "",
-#     "pink_light": "",
-#     "black_light": ""
-#   },
-#   "limits": {}
-#}
