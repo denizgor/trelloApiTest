e4026d6367c74b2b2ad39fa25f43cf3b775931a7
Deniz-Insider <deniz.gormez@useinsider.com>
2024-02-10 20:48:33 +0300

    - Insider v.2

--- base/base_urls.py
+++ base/base_urls.py
@@ -1,6 +1,6 @@
 class BaseUrls:
     MAIN_URL = "https://api.trello.com/1"
     BOARDS_URL = MAIN_URL + "/boards"
-    CREATE_LIST_URL = BOARDS_URL + "/{}/lists".format("")
-    LISTS_URL = MAIN_URL + "/lists{}".format("")
+    CREATE_LIST_URL = BOARDS_URL + "/{}/lists".format(id)
+    LISTS_URL = BOARDS_URL + "/lists/{}".format(id)
     CARDS_URL = MAIN_URL + "/cards"
--- tests/test_create_list.py
+++ tests/test_create_list.py
@@ -4,40 +4,56 @@ from base.access_token import AccessToken
 from base.base_payloads import BasePayload
 import json
 
+
 class TestCreateList(Base):
+    #BOARD PAYLOAD
+    board_payload = {"name": "Test_List",
+                     "key": base.access_token.AccessToken.API_KEY,
+                     "token": base.access_token.AccessToken.ACCESS_TOKEN
 
-    board_payload = { "name": "Test",
-                      "key": base.access_token.AccessToken.API_KEY,
-                      "token": base.access_token.AccessToken.ACCESS_TOKEN
-
-    }
-
-    def test_create_board(self):
-        response = self.general_request(method="POST", url=base.base_urls.BaseUrls.BOARDS_URL,
-                                        payload= self.board_payload)
-        formatted_response = json.dumps(response.json(), indent=2)
-        print(formatted_response)
-        board_id = response.json()["id"]
-        print(board_id)
-        return str(board_id)
+                     }
 
     headers = {
         "Accept": "application/json"
     }
 
+
+    def test_create_list(self):
+        response = self.general_request(method="POST", url=base.base_urls.BaseUrls.BOARDS_URL,
+                                        payload=self.board_payload)
+        formatted_response = json.dumps(response.json(), indent=2)
+        print(formatted_response)
+        board_id = str( response.json()["id"])
+        print("Board ID: ", board_id)
+
+
+        # CREATE LIST PAYLOAD
+        query = {
+            "name": "Test_List3",
+            "idBoard": board_id,
+            "key": base.access_token.AccessToken.API_KEY,
+            "token": base.access_token.AccessToken.ACCESS_TOKEN
+        }
+        # Create a new list
+        response = self.general_request(method="POST", url=base.base_urls.BaseUrls.CREATE_LIST_URL.format(board_id),  params=query)
+
+        print(response)
+        print(response.text)
+
+        query = {
+
+            "key": base.access_token.AccessToken.API_KEY,
+            "token": base.access_token.AccessToken.ACCESS_TOKEN,
+            "idBoard": board_id
+        }
+        response = self.general_request(method="GET", url=base.base_urls.BaseUrls.LISTS_URL, params=query)
+        print("Get List: ", response.json)
+        print(response.status_code)
+        print(response.text)
+
     query = {
         "name": "Test_List2",
+        "idBoard": "65c568118c53a0b61181904a",
         "key": base.access_token.AccessToken.API_KEY,
         "token": base.access_token.AccessToken.ACCESS_TOKEN
     }
-
-    def test_create_new_list(self):
-        response = self.general_request(method="POST", url=base.base_urls.BaseUrls.CREATE_LIST_URL.format(board_id= self.test_create_board()),
-                                        headers=self.headers, payload=self.query)
-
-        print(response)
-
-
-    def test_get_lists(self):
-        response = self.general_request(method="POST", url="https://api.trello.com/1/boards/65b6cfef61edf7fc0a32fea2/lists", headers=self.headers, payload=self.query)
-        print(response.json)
