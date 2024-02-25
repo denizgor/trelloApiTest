070d4aebc92146bf022c0e6e006323dd89374b7b
denizgor <71087556+denizgor@users.noreply.github.com>
2024-02-25 22:15:47 +0300

    - create list test class fixed

--- tests/test_create_list.py
+++ tests/test_create_list.py
@@ -3,57 +3,65 @@ from base.base_functions import Base
 from base.access_token import AccessToken
 from base.base_payloads import BasePayload
 import json
+import logging
 
 
 class TestCreateList(Base):
-    #BOARD PAYLOAD
-    board_payload = {"name": "Test_List",
-                     "key": base.access_token.AccessToken.API_KEY,
-                     "token": base.access_token.AccessToken.ACCESS_TOKEN
-
-                     }
-
-    headers = {
-        "Accept": "application/json"
-    }
-
 
     def test_create_list(self):
-        response = self.general_request(method="POST", url=base.base_urls.BaseUrls.BOARDS_URL,
-                                        payload=self.board_payload)
+        # BOARD PAYLOAD
+        board_payload = {"name": "Test_List_Board",
+                         "key": base.access_token.AccessToken.API_KEY,
+                         "token": base.access_token.AccessToken.ACCESS_TOKEN
+
+                         }
+
+        headers = {
+            "Accept": "application/json"
+        }
+
+        #Create Board
+        response = self.general_request(method="POST", url=base.base_urls.BaseUrls.BOARDS_URL, headers=headers,
+                                        payload=board_payload)
         formatted_response = json.dumps(response.json(), indent=2)
         print(formatted_response)
-        board_id = str( response.json()["id"])
+        board_id = response.json()["id"]
         print("Board ID: ", board_id)
+
+        self.assertEqual(response.status_code, 200, "Status code is NOT 200!")
+        self.assertEqual(response.headers["Content-Type"], "application/json; charset=utf-8", "Incorrect JSON format!")
+
+
 
 
         # CREATE LIST PAYLOAD
         query = {
-            "name": "Test_List3",
+            "name": "Test_List1",
             "idBoard": board_id,
             "key": base.access_token.AccessToken.API_KEY,
             "token": base.access_token.AccessToken.ACCESS_TOKEN
         }
+
+        #log
+        #logging.basicConfig(level=logging.DEBUG)
+
         # Create a new list
-        response = self.general_request(method="POST", url=base.base_urls.BaseUrls.CREATE_LIST_URL.format(board_id),  params=query)
+        response = self.general_request(method="POST", url=base.base_urls.BaseUrls.LIST_URL, params=query)
+        list_id = response.json()["id"]
 
         print(response)
-        print(response.text)
+        print("List ID: ", list_id)
+        print("Create List Response Text: ", response.text)
+
+        #Get List
 
         query = {
 
             "key": base.access_token.AccessToken.API_KEY,
             "token": base.access_token.AccessToken.ACCESS_TOKEN,
-            "idBoard": board_id
         }
-        response = self.general_request(method="GET", url=base.base_urls.BaseUrls.LISTS_URL, params=query)
-        print("Get List: ", response.json)
+        response = self.general_request(method="GET", url=base.base_urls.BaseUrls.GET_LIST_URL.format(list_id), params=query)
+        print("Get List: ", response.json())
         print(response.status_code)
         print(response.text)
 
-    query = {
-        "name": "Test_List2",
-        "idBoard": "65c568118c53a0b61181904a",
-        "key": base.access_token.AccessToken.API_KEY,
-        "token": base.access_token.AccessToken.ACCESS_TOKEN
-    }
